31b5eed1dc1c65ca6dbd7a76338208f5a7b5c307
funkiben <funkiben@gmail.com>
2014-11-10 17:42:17 -0500

    Test generator now generates trees

--- src/server/world/TestWorldGenerator.java
+++ src/server/world/TestWorldGenerator.java
@@ -1,5 +1,6 @@
 package server.world;
 
+import net.funkitech.util.Location;
 import server.world.object.WorldObject;
 import server.world.object.WorldObjectType;
 
@@ -14,27 +15,27 @@ public class TestWorldGenerator extends WorldGenerator {
 		
 		WorldObject object;
 		
+		int tileSize = 50;
 		
-		for (int i = 100; i <= 700; i+= 100) {
-			object = new WorldObject(chunk.getLocation().add(i, 100), WorldObjectType.BLOCK);
-			chunk.addObject(object);
-			
-			object = new WorldObject(chunk.getLocation().add(i, 700), WorldObjectType.BLOCK);
-			chunk.addObject(object);
-			
-			object = new WorldObject(chunk.getLocation().add(700, i), WorldObjectType.BLOCK);
-			chunk.addObject(object);
-			
-			object = new WorldObject(chunk.getLocation().add(100, i), WorldObjectType.BLOCK);
-			chunk.addObject(object);
-			
+		for (int x = 0; x < Chunk.size; x += tileSize) {
+			for (int y = 0; y < Chunk.size; y += tileSize) {
+				object = new WorldObject(new Location(x, y).add(chunk.getLocation()), WorldObjectType.TILE_COBBLE, tileSize, tileSize, (int) (Math.random() * 10));
+				chunk.addObject(object);
+			}
 		}
 		
-//		for (WorldObject obj : chunk.getObjects()) {
-//			if (obj.getChunkX() != chunk.getX() || obj.getChunkY() != chunk.getY()) {
-//				System.out.println("NOPE!");
-//			}
-//		}
+		int trees = (int) (3 + (Math.random() * 5));
+		
+		int treeWidth = 100;
+		int treeHeight = 200;
+		
+		for (int i = 0; i < trees; i++) {
+			Location loc = new Location(Math.random() * Chunk.size, Math.random() * Chunk.size).add(chunk.getLocation());
+			object = new WorldObject(loc, WorldObjectType.TREE, treeWidth, treeHeight, (int) (Math.random() * 4));
+			chunk.addObject(object);
+		}
+		
+		
 		
 		
 	}
